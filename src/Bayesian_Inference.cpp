8693f5c4153318c66ad9f9e60a7a8ad3f5b3fb4b
maartenvandenheuvel <maarten.vandenheuvel@student.uantwerpen.be>
2014-11-08 17:48:47 +0100

    Testing git

---
+++ src/Bayesian_Inference.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Bayesian_Inference.cpp
+// Author      : Maarten Van den Heuvel
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
